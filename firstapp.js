3fbba48fdc63abcf7f74c0c94667e0e3ae5f277f
Albert <salyakh0v@yandex.ru>
2020-03-14 23:25:03 +0300

    second

--- firstapp.js
+++ firstapp.js
@@ -1,30 +1,32 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http');//подключение модуля сервера(уже есть в ноде)
+const fs = require('fs');//подключение модуля файловой системы
 
-const server = http.createServer(function(req, res){
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
+const server = http.createServer(function(req, res){    //создаем сервер+инциализируем функцию, которая будет активироваться с каждым запросом на сервер
+    const url = req.url;//константа, которая будет означать нашу ссылку
+    const method = req.method; 
+    if (url === '/') {//при переходе на ссылку localhost:3000/ будет запускаться код снизу
         res.write('<html>');
-    res.write('</head><title>Enter message</title></head>');
+    res.write('<head><title>Enter message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type ="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
-    return res.end();
+    return res.end();//выходим из функции
     }
     if (url === '/message' && method === 'POST') {
-        const body =[];
-        req.on('data', (chunk) =>  {
+        const body =[];//array with chunks
+        req.on('data', (chunk) =>  { //listen events and fuctioin receve's chunk of data to work with
             console.log(chunk);
-            body.push(chunk);
+            body.push(chunk);//add chunk
         });
         req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1];
-            fs.writeFileSync('message.txt', message);
+            const parsedBody = Buffer.concat(body).toString();//converting chunks to a string
+            const message = parsedBody.split("=")[1];//присваиваем сообщение константе 
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+       
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
